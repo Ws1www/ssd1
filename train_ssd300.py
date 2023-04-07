a64df16f76b8fb37788bd9187d4c0bf78feb5a74
Ws1www <129833341+Ws1www@users.noreply.github.com>
2023-04-07 22:38:38 +0800

    Update train_ssd300.py

--- train_ssd300.py
+++ train_ssd300.py
@@ -175,9 +175,9 @@ if __name__ == '__main__':
     # 检测的目标类别个数，不包括背景
     parser.add_argument('--num_classes', default=20, type=int, help='num_classes')
     # 训练数据集的根目录(VOCdevkit)
-    parser.add_argument('--data-path', default='yg', help='dataset')
+    parser.add_argument('--data-path', default='/kaggle/input/diordata/ImageSets', help='dataset')
     # 文件保存地址
-    parser.add_argument('--output-dir', default='./save_weights', help='path where to save')
+    parser.add_argument('--output-dir', default='/kaggle/working/save_weights', help='path where to save')
     # 若需要接着上次训练，则指定上次训练保存权重文件地址
     parser.add_argument('--resume', default='', type=str, help='resume from checkpoint')
     # 指定接着从哪个epoch数开始训练
